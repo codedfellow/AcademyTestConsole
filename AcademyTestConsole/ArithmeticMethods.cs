0d068bebc4e1fb3cadc511e6f3120ee07e67e2b4
elvis.aghaulor <elvis.aghaulor@fintraksoftware.com>
2023-12-14 10:03:04 +0100

    implemented subtraction

---
+++ AcademyTestConsole/ArithmeticMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyTestConsole
+{
+    internal class ArithmeticMethods
+    {
+        public void PerformSubtraction()
+        {
+            Console.WriteLine("Enter first value");
+            decimal.TryParse(Console.ReadLine(), out decimal firstValue);
+            Console.WriteLine("Enter second value");
+            decimal.TryParse(Console.ReadLine(), out decimal secondValue);
+            decimal sum = firstValue - secondValue;
+            Console.WriteLine($"\nThe difference is {decimal.Round(sum, 2)}");
+        }
+    }
+}
--- AcademyTestConsole/Program.cs
+++ AcademyTestConsole/Program.cs
@@ -8,12 +8,10 @@
             Console.WriteLine("Welcome To academy 2023");
             Console.WriteLine("====================================");
             Console.WriteLine("------------- Mini Calculator ------------\n\n");
-            Console.WriteLine("Enter first value");
-            decimal.TryParse(Console.ReadLine(),out decimal firstValue);
-            Console.WriteLine("Enter second value");
-            decimal.TryParse(Console.ReadLine(), out decimal secondValue);
-            decimal sum = firstValue + secondValue;
-            Console.WriteLine($"\nThe sum is {decimal.Round(sum, 2)}");
+
+            var arithmeticCaller = new ArithmeticMethods();
+            arithmeticCaller.PerformSubtraction();
+
             Console.ReadKey();
         }
     }
