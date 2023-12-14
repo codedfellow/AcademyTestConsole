7227bd18142c4fa7ef0656b9a2b8fcb4034dc279
elvis.aghaulor <elvis.aghaulor@fintraksoftware.com>
2023-12-14 09:51:53 +0100

    first commit: addition project

---
+++ AcademyTestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AcademyTestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====================================");
+            Console.WriteLine("Welcome To academy 2023");
+            Console.WriteLine("====================================");
+            Console.WriteLine("------------- Mini Calculator ------------\n\n");
+            Console.WriteLine("Enter first value");
+            decimal.TryParse(Console.ReadLine(),out decimal firstValue);
+            Console.WriteLine("Enter second value");
+            decimal.TryParse(Console.ReadLine(), out decimal secondValue);
+            decimal sum = firstValue + secondValue;
+            Console.WriteLine($"\nThe sum is {decimal.Round(sum, 2)}");
+            Console.ReadKey();
+        }
+    }
+}
